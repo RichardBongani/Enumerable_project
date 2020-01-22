b7e1e1d354573b602c4c738aeb72e02116f7743e
Richard <phalarichard1@gmail.com>
2020-01-22 21:29:36 +0200

    fixing errors suggested by tse

--- enumerables.rb
+++ enumerables.rb
@@ -64,13 +64,13 @@ module Enumerable
 
     def my_map(proc= nil)
         array = []
-        for i in 0..self.length
+        for i in 1..self.length
             yield(self[i])
             array.push(i)
         end
         array
     end
-
+    
     def my_inject(total = nil)
         total = self[0] unless total
         for a in 1..self.length - 1
@@ -86,7 +86,7 @@ def multiply_els(arr)
     arr.my_inject( :*)
 end
 
-my_proc = proc.new{|x| x**2}
+my_proc = Proc.new{|x| x}
 
 [1,2,3,4,5].my_each {|x| p x}
 [1,2,3,4,5].my_each_with_index {|value,index| p "#{index} with a value of #{value}"}
@@ -96,6 +96,6 @@ p [1,2,3,4,8].my_select {|item| item.odd?}
 [1,2,3,4,5].my_none?{|element| p element.even?}
 p [1,2,3,4,5].my_count{|item| item < 5}
 p [1,2,3,4,5].my_map{|x|  x }
-p [1,2,3,4,5].my_map(my_proc)
+p [1,2,3,4,5].my_map(&my_proc)
 [2,4,5].my_inject{|total,num1| p total*num1}
 
