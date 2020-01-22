12cc66eb04fd3a70fd4e452eae35ac34975a780a
Richard <phalarichard1@gmail.com>
2020-01-22 19:08:18 +0200

    add more functions and testing them

--- enumerables.rb
+++ enumerables.rb
@@ -11,7 +11,50 @@ module Enumerable
             yield(self[i],i)
         end
     end
+
+    def my_select
+        arr = []
+        for i in 1..self.length
+            if yield(i)
+                arr.push(i)
+            end
+        end
+        arr
+    end
+
+    def my_all?
+        results = true
+        for i in 0..self.length-1
+            if yield(self[i])
+                results = true
+            end
+        end
+        results
+    end
+
+    def my_any?
+        results = true
+        for i in 0..self.length-1
+            if yield(self[i])
+                results = true
+            end
+        end
+        results
+    end
+
+    def my_none?
+        results = true
+        for i in 0..self.length-1
+            if yield(self[i])
+                results = false
+            end
+        end
+        results
+    end
 end
 [1,2,3,4,5].my_each {|x| p x}
 [1,2,3,4,5].my_each_with_index {|value,index| p "#{index} with a value of #{value}"}
-
+p [1,2,3,4,8].my_select {|item| item.odd?}
+[1,2,6,4,5].my_all?{|element| p element.even?}
+[1,2,3,4,5].my_any?{|item| p item.odd?}
+[1,2,3,4,5].my_none?{|element| p element.even?}
