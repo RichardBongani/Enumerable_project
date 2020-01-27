6c513c98ea582b3a4f42b150d99ea882001a9c24
Richard <phalarichard1@gmail.com>
2020-01-28 00:51:17 +0200

    fixing errors and testing

--- enumerables.rb
+++ enumerables.rb
@@ -2,35 +2,36 @@ module Enumerable
 
   def my_each
     if block_given?
-      (0..length - 1).each do |i|
+      i = 0
+      while i < self.size
         yield(self[i])
+        i += 1
       end
       self
-    else
-      Enumerable
     end
   end
 
   def my_each_with_index
     if block_given?
-      (0..length - 1).each do |i|
-        yield(self[i], i)
+      i = 0
+      while i < self.size
+        yield(self[i],i)
+        i += 1
       end
-    else
-      Enumerable
+      self
     end
   end
 
   def my_select
-    arr = []
     if block_given?
-      (1..length - 1).each do |i|
-        arr.push(self[i]) if yield(self[i])
+      array = []
+      i = 0
+      while i < self.length
+        array << self[i] if yield(i)
+            self[i] += 1
       end
-    else
-      Enumerable
+      array
     end
-    arr
   end
 
   def my_all?
@@ -96,14 +97,14 @@ end
 
 my_proc = proc { |x| x }
 
-[1, 2, 3, 4, 5].my_each { |x| p x }
-[1, 2, 3, 4, 5].my_each_with_index { |value, index| p "#{index} with a value of #{value}" }
-p [1, 2, 3, 4, 8].my_select(&:even?)
-p [1, 2, 6, 4, 5].my_all? { |element| element < 15 }
-p [1, -2, 3, 4, 5].my_any?(&:negative?)
-p [4, 4, 6, 4, 10].my_none? { |element| element > 100 }
-p [1, 2, 3, 4, 6].my_count { |item| (item % 3).zero? }
-p [1, 2, 3, 4, 5].my_map { |number| number * number }
-p [1, 2, 3, 4, 5].my_map(&my_proc)
-p [2, 4, 5].my_inject { |total, num1| total * num1 }
-p multiply_els([2, 4, 5])
+# [1, 2, 3, 4, 5].my_each { |x| p x }
+# [1, 2, 3, 4, 5].my_each_with_index { |value, index| p "#{index} with a value of #{value}" }
+# p [1, 2, 3, 4, 8].my_select(&:even?)
+# p [1, 2, 6, 4, 5].my_all? { |element| element < 15 }
+# p [1, 2, 3, 4, 5].my_any?(&:negative?)
+# p [1, 2, 3, 4, 5].my_none? { |element| element > 100 }
+# p [1, 2, 3, 4, 5].my_count { |item| (item % 3).zero? }
+# p [1, 2, 3, 4, 5].my_map { |number| number * number }
+# p [1, 2, 3, 4, 5].my_map(&my_proc)
+# p [2, 4, 5].my_inject { |total, num1| total * num1 }
+# p multiply_els([2, 4, 5])
---
+++ testing.rb
@@ -0,0 +1,60 @@
+require_relative "enumerables.rb"
+
+
+  a = [1,2,3,4,5]
+  b = [2,4,5]
+
+p "==============================================="
+  p [1, 2, 3, 4, 5].my_each { |x| x }
+  p "**********************************************"
+  p a.each {|n| n }
+  p "//////////////////MY EACH////////////////////////////"
+  p [1, 2, 3, 4, 5].my_each { |x| x } ==  a.each {|n| n }
+  p "============================================="
+    [1, 2, 3, 4, 5].my_each_with_index { |value, index| p "my ind is #{index} with a value of #{value}" }
+  p "***********************************************"
+    a.each_with_index {|val, ind| p  "my index is #{ind} with val of #{val}"}
+  p "////////////////////EACH WITH INDEX////////////////////////////"
+  p [1, 2, 3, 4, 5].my_each_with_index { |value, index|  "#{index} with a value of #{value}" } == a.each_with_index {|val, ind| "my index is #{ind} with val of #{val}"}
+  p "================================================"
+  p [1, 2, 3, 4, 5].my_select{|item| item.even?}
+  p "************************************************"
+  p a.select { |n| n.odd?}
+  p "//////////////////SELECT//////////////////////////////"
+  p [1, 2, 3, 4, 5].my_select(&:even?) ==  a.select { |n| n.even?}
+  p "================================================"
+  p [1, 2, 6, 4, 5].my_all? { |element| element < 15 }
+  p "*************************************************"
+  p a.all? {|element| element < 15}
+  p "///////////////////ALL//////////////////////////////"
+  p [1, 2, 6, 4, 5].my_all? { |element| element < 15 } == a.all? {|element| element < 15}
+  p "================================================="
+  p [1, 2, 3, 4, 5].my_any?(&:negative?)
+  p "*************************************************"
+  p a.any?{ |item| item.negative?}
+  p "////////////////ANY//////////////////////////////"
+  p [1, 2, 3, 4, 5].my_any?(&:negative?) == a.any?{ |item| item.negative?}
+  p "================================================="
+  p [1, 2, 3, 4, 5].my_none? { |element| element > 100 }
+  p "**************************************************"
+  p a.none? {|element| element > 100}
+  p "/////////////////////NONE/////////////////////////"
+  p [1, 2, 3, 4, 5].my_none? { |element| element > 100 } == a.none? {|element| element > 100}
+  p "=================================================="
+  p [1, 2, 3, 4, 5].my_count { |item| (item % 2).zero? }
+  p "**************************************************"
+  p a.count {|item| (item%2).zero?}
+  p "//////////////////////COUNT////////////////////////"
+  p [1, 2, 3, 4, 5].my_count { |item| (item % 2).zero? } == a.count {|item| (item%2).zero?}
+  p "==================================================="
+  p [1, 2, 3, 4, 5].my_map { |number| number * number }
+  p "***************************************************"
+  p a.map {|num| num * num}
+  p "//////////////////////MAP///////////////////////////"
+  p [1, 2, 3, 4, 5].my_map { |number| number * number } == a.map {|num| num * num}
+  p "===================================================="
+  p p [2, 4, 5].my_inject { |total, num1| total * num1 }
+  p "****************************************************"
+  p b.inject {|tot, val| tot*val}
+  p "//////////////////////////INJECT/////////////////////"
+  p p [2, 4, 5].my_inject { |total, num1| total * num1 } == b.inject {|tot, val| tot*val}
